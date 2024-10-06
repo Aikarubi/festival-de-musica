554fab8cf0b419aaf0fdd791163038047692f7c2
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-06 10:11:01 +0200

    gulpfile

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ npm i gulp
 
 
 npm i para instalar todas las dependencias registradas en package
+
+
+scripts:
+
+npm run sass
---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+export function hola( done ) {
+    console.log('Hola desde Gulpfile.js');
+
+    done(); // Callback que se ejecuta cuando finalice el gulp
+}
