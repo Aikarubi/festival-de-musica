e605224da509b59072bf4ec632207e1d4911c704
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-10 13:48:51 +0200

    mejorando modal

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     crearGaleria();
 
@@ -14,6 +14,42 @@ function crearGaleria() {
         imagen.src = `src/img/gallery/full/${i}.jpg`;
         imagen.alt = `Imagen Galería ${i}`;
 
+        // Event Handler
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(i) {
+
+    // Generar modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.onclick = cerrarModal;
+
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = `Imagen Galería ${i}`;
+    modal.appendChild(imagen);
+
+    // Agregar al HTML
+    const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
+    body.appendChild(modal);
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+        modal?.remove();
+        
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500);
+
 }
