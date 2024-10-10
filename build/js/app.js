7743a8c20567de1caac54a56cf8fd559ba40484f
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-10 12:59:36 +0200

    listando las imagenes

---
+++ build/js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    crearGaleria();
+
+});
+
+function crearGaleria() {
+
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG');
+        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.alt = `Imagen Galería ${i}`;
+
+        galeria.appendChild(imagen);
+    }
+}
