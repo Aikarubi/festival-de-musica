4efed01e60eef2081113c8b9667bd6ee4a77b130
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-10 16:48:23 +0200

    Mejorando el scroll

--- build/js/app.js
+++ build/js/app.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     navegacionFija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 
 });
 
@@ -16,6 +18,30 @@ function navegacionFija() {
             header.classList.remove('fixed');
         }
     });
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function () {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+        let actual = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (window.scrollY >= ( sectionTop - sectionHeight / 3 ) ) {
+                actual = section.id;
+            }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if(link.getAttribute('href') === '#' + actual) {
+            link.classList.add('active');
+        }
+    });
+});
 }
 
 function crearGaleria() {
@@ -74,4 +100,19 @@ function cerrarModal() {
         body.classList.remove('overflow-hidden');
     }, 500);
 
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({ behavior: 'smooth' });
+            
+        });
+    });
 }
