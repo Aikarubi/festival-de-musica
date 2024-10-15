ac3aff6a144f075412dc0e92a04f4359dae1053b
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-15 11:31:39 +0200

    hacer las imagenes más pequeñas

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import fs from 'fs';
 import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -14,6 +16,7 @@ import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
 
 import terser from 'gulp-terser';
+import sharp from 'sharp';
 
 export function js( done ) {
 
@@ -34,9 +37,37 @@ export function css( done ) {
     done();
 }
 
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export function dev() {
     watch('src/scss/**/*.scss', css);
     watch('src/js/**/*.js', js); //Esta ruta es para leer todos los archivos .scss que esten en esta carpeta y sus subcarpetas
 }
 
-export default series( js, css, dev );
+export default series( crop, js, css, dev );
